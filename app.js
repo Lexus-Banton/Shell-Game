ed8fe98964c04cc958f537610d44837cd4391e62
Lexus Banton <lexusbanton.kr@gmail.com>
2022-09-12 17:10:11 -0700

    Got shells down

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 /* Imports */
-// import { getRandomItem } from './utils.js';
+import { getRandomItem } from './utils.js';
 
 /* State */
+let gameState = 'guess';
 
 /* Actions */
-function loadPage() {}
+function loadPage() {
+    displayShells();
+}
 
 /* Components */
+const shellOne = document.getElementById('shell-1');
+const shellTwo = document.getElementById('shell-2');
+const shellThree = document.getElementById('shell-3');
 
+const pearlOne = document.getElementById('pearl-1');
+const pearlTwo = document.getElementById('pearl-2');
+const pearlThree = document.getElementById('pearl-3');
+
+const guessDiv = document.getElementById('guesses');
+const resultDiv = document.getElementById('results');
+const playAgainBtn = document.getElementById('play-again-button');
 /* Component */
 // get DOM
 // display
+function displayShells() {
+    if (gameState === 'guess') {
+        shellOne.classList.remove('reveal');
+        shellTwo.classList.remove('reveal');
+        shellThree.classList.remove('reveal');
+        resultDiv.classList.add('hidden');
+    } else {
+    }
+}
 // event listeners
 
 /* Run page load code */
